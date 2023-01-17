c723a394e84f2f09b377a6499095a17e02269445
deka ihza maulana 1b <dekaihzamaulana@gmail.com>
2023-01-17 09:13:08 +0700

    jawaban saya

---
+++ src/SumOfNumber.java
@@ -0,0 +1,12 @@
+public class SumOfNumber {
+    public static void main(String[] args) {
+      int angka1 = 2;
+      int  angka2 = 3;
+      int angka3 = 4;
+      int jumlah = angka1 + angka2 + angka3;
+        System.out.println(jumlah);
+
+
+    }
+}
+
